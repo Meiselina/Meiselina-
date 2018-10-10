8daaf4157089706ab371c3d44f3832af7ebc9564
Meiselina <43603565+Meiselina@users.noreply.github.com>
2018-10-10 14:26:27 +0800

    Add files via upload

---
+++ ex13inputprompt.py
@@ -0,0 +1,18 @@
+from sys import argv
+
+script, first, second, third = argv
+prompt = '> '
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+
+print("What's your name?")
+Name = input(prompt)
+
+print (""")
+My english name is Mei
+My chinese name is Midi
+Wo de xing Du
+(""")
