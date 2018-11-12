74677d16e948a89c81e61bec7d26d2a356ed0dbd
Meiselina <43603565+Meiselina@users.noreply.github.com>
2018-11-13 05:52:14 +0800

    Add files via upload

---
+++ Ex5.py
@@ -0,0 +1,19 @@
+# -- coding: utf- 8 -- 
+
+my_name = 'Meiselina'
+my_age = 28 # not a lie
+my_height = 159 # centimeters
+my_weight = 50 # kilograms
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Wear Vail/Black'
+
+print (f"Let's talk about {my_name}.")
+print (f"He's {my_height} inches tall.")
+print (f"He's {my_weight} pounds heavy.")
+print ("Actually that's not too heavy.")
+print (f"He's got {my_eyes} eyes and {my_hair} hair.")
+print (f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+print (f"If I add {my_age}, {my_height}, and {my_weight} I get {my_age + my_height + my_weight}.")
